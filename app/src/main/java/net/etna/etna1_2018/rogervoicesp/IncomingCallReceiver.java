65ca0fb9c31fcfe38be1389c9ec42712f5ac81fd
Ludovic Lefebure <ludovic.lefebure@revevol.eu>
2015-12-09 12:05:12 +0100

    Notification triggered on new outgoing call

--- app/src/main/java/net/etna/etna1_2018/rogervoicesp/IncomingCallReceiver.java
+++ app/src/main/java/net/etna/etna1_2018/rogervoicesp/IncomingCallReceiver.java
@@ -1,8 +1,13 @@
 package net.etna.etna1_2018.rogervoicesp;
 
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
 
 /**
@@ -10,8 +15,41 @@ import android.util.Log;
  */
 public class IncomingCallReceiver extends BroadcastReceiver {
 
+    private NotificationManager mNM;
+
+    // Unique Identification Number for the Notification.
+    // We use it on Notification start, and to cancel it.
+    private int NOTIFICATION = R.string.local_service_started;
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d("Entered", "OUIIIII");
+        Log.d("Call", "Entered onReceive");
+        mNM = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        // context.startService(new Intent());
+        showNotification(context);
     }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    private void showNotification(Context context) {
+        // In this sample, we'll use the same text for the ticker and the expanded notification
+        CharSequence text = context.getText(R.string.local_service_started);
+
+        // The PendingIntent to launch our activity if the user selects this notification
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
+                new Intent(context, MainActivity.class), 0);
+
+        // Set the info for the views that show in the notification panel.
+        Notification notification = new Notification.Builder(context)
+                .setSmallIcon(R.mipmap.ic_launcher)  // the status icon
+                .setTicker(text)  // the status text
+                .setWhen(System.currentTimeMillis())  // the time stamp
+                .setContentTitle(context.getText(R.string.local_service_label))  // the label of the entry
+                .setContentText(text)  // the contents of the entry
+                .setContentIntent(contentIntent)  // The intent to send when the entry is clicked
+                .build();
+
+        // Send the notification.
+        mNM.notify(NOTIFICATION, notification);
+    }
+
 }
